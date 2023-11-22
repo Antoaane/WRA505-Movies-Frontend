dd7efa38f6f99027595de1e168dfb4f9861cf3f1
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2023-11-22 16:56:40 +0100

    feat : edit movie

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -6,14 +6,18 @@
     let recherche = ref('');
     let dataFull = ref('');
 
-    onMounted(async () => {
+    async function getFilms() {
         const response = await axios.get('http://localhost/index.php/api/movies', {
             headers: {
-            'Accept': 'application/json'
+                'Accept': 'application/json'
             }
         });
         data.value = response.data;
         dataFull.value = response.data;
+    }
+
+    onMounted(() => {
+        getFilms();
     });
 
     console.log(data);
@@ -22,10 +26,46 @@
         filtrerPays();
     });
 
+
+    let editedMovieId = ref('');
+    let filmTitle = ref('');
+
     function filtrerPays() {
         if (dataFull.value) {
             data.value = dataFull.value.filter(movie => movie.title.toLowerCase().includes(recherche.value.toLowerCase()));
         }
+    }
+
+    function formShow(currentId) {
+        document.querySelector('.form-container').style.display = 'flex';
+
+        filmTitle.value = data.value.find(movie => movie.id == currentId).title;
+        editedMovieId.value = currentId;
+    }
+
+    function formHide() {
+        document.querySelector('.form-container').style.display = 'none';
+        editedMovieId.value = '';
+        filmTitle.value = '';
+    }
+
+    function editMovieTitle(movieId) {
+        axios.patch(`http://localhost/index.php/api/movies/${movieId}`, {
+            title: filmTitle.value
+        }, {
+            headers: {
+                'Content-Type': 'application/merge-patch+json'
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+            getFilms();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+        formHide();
     }
 </script>
 
@@ -34,10 +74,44 @@
         <h1>Films</h1>
         <input type="text" v-model="recherche">
         <button @click="filtrerPays()">Rechercher</button>
-        <router-link :to="`/movies/${movie.id}`" v-for="movie in data" :key="movie.id">
-            {{ movie.title }} <br>
-            {{ movie.description }} <br>
-            {{ movie.releaseDate }} <br>
-        </router-link>
+        <div class="film-list-item" v-for="movie in data" :key="movie.id">
+            <router-link :to="`/movies/${movie.id}`">
+                {{ movie.title }} <br>
+                {{ movie.description }} <br>
+                {{ movie.releaseDate }} <br>
+            </router-link>
+            <button class="btn" @click="formShow(movie.id)">
+                modifier
+            </button>
+        </div>
+        <div class="form-container">
+            <form @submit.prevent="editMovieTitle(editedMovieId)">
+                <input 
+                    class="form-input"
+                    type="text"
+                    name="title"
+                    id="title"
+                    v-model="filmTitle"
+                >
+                <button type="submit" class="btn">
+                    Modifier
+                </button>
+            </form>
+            
+            <svg
+                @click="formHide()"
+                class="cross"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24">
+                <path
+                    class="cross-path"
+                    fill="rgb(78, 127, 131)"
+                    d="M12 10.586L3.707 2.293 2.293 3.707 10.586 12l-8.293 8.293 1.414 1.414L12 13.414l8.293 8.293 1.414-1.414L13.414 12l8.293-8.293-1.414-1.414z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </div>
     </section>
 </template>
